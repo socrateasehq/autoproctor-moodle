99c132292ff738de8ed9c036f16d2250cecc8f96
Faizan Jan <faizan@equip.co>
2024-11-11 15:49:29 +0530

    add lang strings for tracking options

--- lang/en/quizaccess_autoproctor.php
+++ lang/en/quizaccess_autoproctor.php
@@ -10,3 +10,15 @@ $string['enable_by_default'] = 'Enable AutoProctor by default';
 $string['enable_by_default_desc'] = 'If enabled, AutoProctor will be enabled for all new quizzes by default';
 $string['requireautoproctor'] = 'Require AutoProctor';
 $string['requireautoproctor_help'] = 'If enabled, students can only attempt the quiz when AutoProctor is running';
+
+// Proctoring options
+$string['tracking_audio'] = 'Audio Detection';
+$string['tracking_numHumans'] = 'Number of Humans';
+$string['tracking_tabSwitch'] = 'Detect Tab Switch';
+$string['tracking_captureSwitchedTab'] = 'Capture Switched Tab';
+$string['tracking_photosAtRandom'] = 'Take photos at random';
+$string['tracking_recordSession'] = 'Record Users screen session';
+$string['tracking_detectMultipleScreens'] = 'Detect Multiple Screens';
+$string['tracking_testTakerPhoto'] = 'Take photo of test taker in the beginning of the test';
+$string['tracking_showCamPreview'] = 'Show camera preview';
+$string['tracking_forceFullScreen'] = 'Force full screen';
